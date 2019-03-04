0cf3b3ab6d93a4f6965eb914ab4eea94d49e4cef
Matthew Dal Santo <SudoSaint@gmail.com>
2019-03-03 21:07:11 -0600

    created the masthead component

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -11,8 +11,7 @@ import { StaticQuery, graphql } from "gatsby"
 import { ThemeProvider } from 'styled-components'
 import { Gray as theme } from '../themes/Gray'
 import { Footer, Main } from '../components/Element'
-
-import Header from "../components/gatsby-components/header"
+import { Masthead } from '../components/Masthead'
 import "./layout.css"
 
 const Layout = ({ children }) => (
@@ -29,7 +28,7 @@ const Layout = ({ children }) => (
       `}
       render={data => (
         <>
-          <Header siteTitle={data.site.siteMetadata.title} />
+          <Masthead siteTitle={data.site.siteMetadata.title} height="75px" />
           <div
             style={{
               margin: `0 auto`,
